45b401e27e2f7d76a8e1d0161f5c96015cad53fc
Michel LEVY PROVENCAL <mlevypro@gmail.com>
2023-02-09 10:51:36 +0100

    modif variable env

--- populate-with-questions.py
+++ populate-with-questions.py
@@ -10,7 +10,7 @@ parser.add_argument('output_filename', help='Nom du fichier CSV de sortie')
 args = parser.parse_args()
 
 # Chargement du modèle d'OpenAI
-openai.api_key = os.environ.get('API_KEY')
+openai.api_key = os.environ.get('OPENAIAPI_KEY')
 model_engine = "text-davinci-003"
 
 # Lecture du fichier CSV d'entrée
